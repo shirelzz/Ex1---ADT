385b406af1cc5dae4d55276e117613ca563fb1a7
shirelzz <99799701+shirelzz@users.noreply.github.com>
2023-03-22 15:55:46 +0200

    adding implementation

--- AdptArray.c
+++ AdptArray.c
@@ -1,45 +1,39 @@
-//
-// Created by שיראל זכריה on 22/03/2023.
-//
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "AdptArray.h"
 
 
-////////A.
-
-
-typedef struct AdptArray_
-{
+typedef struct AdptArray_ {
     int ArrSize;
-    PElement* pElemArr;
+    PElement *pElemArr;
     DEL_FUNC delFunc;
     COPY_FUNC copyFunc;
-}AdptArray;
+    PRINT_FUNC printFunc;
+} AdptArray;
 
 
-
-
-PAdptArray CreateAdptArray(COPY_FUNC copyFunc_, DEL_FUNC delFunc_)
-{
-    PAdptArray pArr = (PAdptArray)malloc(sizeof(AdptArray));
+PAdptArray CreateAdptArray(COPY_FUNC copyFunc_, DEL_FUNC delFunc_, PRINT_FUNC printFunc_) {
+    PAdptArray pArr = (PAdptArray) malloc(sizeof(AdptArray));
     if (pArr == NULL)
         return NULL;
     pArr->ArrSize = 0;
     pArr->pElemArr = NULL;
     pArr->delFunc = delFunc_;
     pArr->copyFunc = copyFunc_;
+    pArr->printFunc = printFunc_;
     return pArr;
 }
-////////B.
-Result SetAdptArrayAt(PAdptArray pArr, int idx, PElement pNewElem)
-{
-    PElement* newpElemArr;
+
+Result SetAdptArrayAt(PAdptArray pArr, int idx, PElement pNewElem) {
+    PElement *newpElemArr;
     if (pArr == NULL)
         return FAIL;
 
-    if (idx >= pArr->ArrSize)
-    {
+    if (idx >= pArr->ArrSize) {
 
 // Extend Array
-        if ((newpElemArr = (PElement*)calloc((idx + 1), sizeof(PElement))) == NULL)
+        if ((newpElemArr = (PElement *) calloc((idx + 1), sizeof(PElement))) == NULL)
             return FAIL;
         memcpy(newpElemArr, pArr->pElemArr, (pArr->ArrSize) * sizeof(PElement));
         free(pArr->pElemArr);
@@ -54,7 +48,6 @@ Result SetAdptArrayAt(PAdptArray pArr, int idx, PElement pNewElem)
     pArr->ArrSize = (idx >= pArr->ArrSize) ? (idx + 1) : pArr->ArrSize;
     return SUCCESS;
 }
-//C.
 
 void DeleteAdptArray(PAdptArray pArr) {
     int i;
@@ -66,3 +59,27 @@ void DeleteAdptArray(PAdptArray pArr) {
     free(pArr->pElemArr);
     free(pArr);
 }
+
+PElement GetAdptArrayAt(PAdptArray pArr, int idx) {
+    if (pArr == NULL || idx >= pArr->ArrSize)
+        return NULL;
+    return pArr->pElemArr[idx];
+}
+
+
+int GetAdptArraySize(PAdptArray pArr) {
+    if (pArr == NULL)
+        return -1;
+    return pArr->ArrSize;
+}
+
+
+void PrintDB(PAdptArray pArr) {
+    int i;
+    if (pArr == NULL)
+        return;
+    for (i = 0; i < pArr->ArrSize; ++i) {
+        pArr->printFunc((pArr->pElemArr)[i]);
+    }
+}
+
